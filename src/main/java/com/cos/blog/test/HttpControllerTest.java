40d6923dda76bce6babd564eb007dc4b818ec350
jkb <a85479094@gmail.com>
2021-03-17 23:02:58 +0900

    20210317 maven & lombok

--- src/main/java/com/cos/blog/test/HttpControllerTest.java
+++ src/main/java/com/cos/blog/test/HttpControllerTest.java
@@ -16,6 +16,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HttpControllerTest {
 	
+	private static final String TAG = "HttpControllerTest";
+	
+	@GetMapping("/http/lombok")
+	public String lombokTest() {
+		Member m = new Member(1,"ssar","1234","email");
+		System.out.println(TAG+"getter : "+m.getId());
+		m.setId(5000);
+		System.out.println(TAG+"setter : "+m.getId());
+		return "lombok test 완료";
+	}
+	
 	// http://localhost:8080/http/get
 	// 인터넷 브라우저 요청은 무조건 get 요청밖에 할 수 없다.
 	// 데이터 선택(Select)
@@ -23,7 +34,7 @@ public class HttpControllerTest {
 	@GetMapping("/http/get")
 	public String getTest(Member m) {
 		return "get 요청 : " + m.getId() + m.getUsername() + m.getPassword()+","+ m.getEmail();
-		
+				
 	}
 	
 	@PostMapping("/http/post")
